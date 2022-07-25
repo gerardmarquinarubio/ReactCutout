77cad6e745303e4af129ea9e77b7f52585a57a21
Gerard Marquina Rubio <gerardmarquinarubio@gmail.com>
2022-07-25 11:22:27 +0200

    No more CSS, just inline

--- Cutout.tsx
+++ Cutout.tsx
@@ -4,8 +4,6 @@ import { Property } from 'csstype';
 
 import Color from 'color';
 
-import styles from './Cutout.css';
-
 export interface ICutoutProps extends PropsWithChildren {
     src : string;
     backgroundColor : 'black' | 'white' | string;
@@ -14,16 +12,27 @@ export interface ICutoutProps extends PropsWithChildren {
 
 export default function Cutout({src, backgroundColor, mode, children} : ICutoutProps) {
     return (
-        <div 
-            className={styles.cutout} 
-            style={{ background: backgroundColor}}>
+        <div
+            style={{ 
+                background: backgroundColor, 
+                textAlign: 'center', 
+                position: 'relative',
+                overflow: 'hidden',
+            }}>
             <img 
-            src={src} 
-            className={styles.image} 
-            style={{ mixBlendMode: (mode !== undefined) ? mode : (Color(backgroundColor).isDark()) ? 'darken' : 'lighten'}} />
-            <span 
-            className={styles.text} 
-            style={{color: (Color(backgroundColor).isDark()) ? 'white' : 'black'}}>
+            src={src}
+            style={{ 
+                mixBlendMode: (mode !== undefined) ? mode : (Color(backgroundColor).isDark()) ? 'darken' : 'lighten',
+                position: 'absolute',
+                top: 0,
+                left: 0,
+                width: '100%',
+                }} />
+            <span
+            style={{
+                color: (Color(backgroundColor).isDark()) ? 'white' : 'black',
+                textOverflow: 'hidden'
+                }}>
                 { children }
             </span>
         </div>
--- dist/Cutout.js
+++ dist/Cutout.js
@@ -1,8 +1,21 @@
 import React from 'react';
 import Color from 'color';
-import styles from './Cutout.css';
 export default function Cutout({ src, backgroundColor, mode, children }) {
-    return (React.createElement("div", { className: styles.cutout, style: { background: backgroundColor } },
-        React.createElement("img", { src: src, className: styles.image, style: { mixBlendMode: (mode !== undefined) ? mode : (Color(backgroundColor).isDark()) ? 'darken' : 'lighten' } }),
-        React.createElement("span", { className: styles.text, style: { color: (Color(backgroundColor).isDark()) ? 'white' : 'black' } }, children)));
+    return (React.createElement("div", { style: {
+            background: backgroundColor,
+            textAlign: 'center',
+            position: 'relative',
+            overflow: 'hidden',
+        } },
+        React.createElement("img", { src: src, style: {
+                mixBlendMode: (mode !== undefined) ? mode : (Color(backgroundColor).isDark()) ? 'darken' : 'lighten',
+                position: 'absolute',
+                top: 0,
+                left: 0,
+                width: '100%',
+            } }),
+        React.createElement("span", { style: {
+                color: (Color(backgroundColor).isDark()) ? 'white' : 'black',
+                textOverflow: 'hidden'
+            } }, children)));
 }
